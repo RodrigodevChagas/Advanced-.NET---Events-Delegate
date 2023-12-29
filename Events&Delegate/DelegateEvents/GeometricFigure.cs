7e0c9150c1d99f3af88e51bfc812dc3441485639
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-12-29 17:36:31 -0300

    Adicionar arquivos de projeto.

---
+++ Events&Delegate/DelegateEvents/GeometricFigure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Delegate.DelegateEvents
+{
+    public delegate void CalculateVolume(double height, double width, double depth);
+
+    public class GeometricFigure
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
+
+        public event CalculateVolume? CalculateVolume;
+        public void EventHandler()
+        {
+            CalculateVolume?.Invoke(Height, Width, Depth);
+        }
+    }
+}
---
+++ Events&Delegate/Program.cs
@@ -0,0 +1,21 @@
+﻿using Events_Delegate.DelegateEvents;
+
+GeometricFigure geometricFigure = new GeometricFigure() 
+{
+    Height = 10,
+    Width = 15,
+    Depth = 20
+};
+
+geometricFigure.CalculateVolume += CalculateSquareVolume;
+geometricFigure.EventHandler();
+
+static void CalculateSquareVolume(double height, double width, double depth)
+{
+    double volume = height * width * depth;
+    Console.WriteLine("Event triggered by client class");
+    Console.WriteLine(volume);
+}
+
+// The use of Delegate is valuable in situations when the logic of execution of a generic method can vary and need to
+// be injected dynamically
